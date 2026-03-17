7f371f3c4994f1480ec29888dc03bc187b6a1f72
Ahmad Falahi <ahmadfalahi1285@gmail.com>
2026-03-18 04:14:39 +0700

    Praktikum 5.2

---
+++ app/Filament/Resources/Users/Schemas/UserForm.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Filament\Resources\Users\Schemas;
+
+use Filament\Schemas\Schema;
+use Filament\Forms\Components\TextInput;
+
+class UserForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+                TextInput::make('email')
+                    ->required()
+                    ->email()
+                    ->maxLength(255),
+                TextInput::make('password')
+                    ->required()
+                    ->password()
+            ]);
+    }
+}
